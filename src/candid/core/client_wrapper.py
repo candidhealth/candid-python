7e773d0917566b611aa6b0afb9898fffa2f4a348
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-10 21:42:09 +0000

    Release 1.3.1

--- src/candid/core/client_wrapper.py
+++ src/candid/core/client_wrapper.py
@@ -21,10 +21,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "candidhealth/0.0.0",
+            "User-Agent": "candidhealth/1.3.1",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "candidhealth",
-            "X-Fern-SDK-Version": "0.0.0",
+            "X-Fern-SDK-Version": "1.3.1",
         }
         token = self._get_token()
         if token is not None:
